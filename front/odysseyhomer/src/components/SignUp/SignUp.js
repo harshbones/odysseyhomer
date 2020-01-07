dcd1e09c5140e44de2264e58ff8d076c1931e6e6
harshbones <davidrangeard@gmail.com>
2020-01-07 10:08:15 +0100

    quest020

--- front/odysseyhomer/src/components/SignUp/SignUp.js
+++ front/odysseyhomer/src/components/SignUp/SignUp.js
@@ -1,16 +1,32 @@
 import React, { useState } from "react";
 
 const Signup = () => {
-  const [email, setEmail] = useState("test@test.com");
+  const [info, setInfo] = useState({
+    email: "bilbon@email.com",
+    password: "myPrecious",
+    name: "Frodon",
+    lastname: "Sacquet"
+  });
 
-  const updateEmailField = e => {
-    setEmail(e.target.value);
+  const updateForm = e => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
+
+  const submitForm = e => {
+    e.preventDefault();
+    console.log(info);
   };
 
   return (
     <div>
-      <h1>{email}</h1>
-      <input type="email" name="email" onChange={updateEmailField} />
+      <h1>{JSON.stringify(info, 1, 1)}</h1>
+      <form className="form" onSubmit={submitForm}>
+        <input type="text" name="name" onChange={updateForm} />
+        <input type="text" name="lastname" onChange={updateForm} />
+        <input type="email" name="email" onChange={updateForm} />
+        <input type="password" name="password" onChange={updateForm} />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 };
